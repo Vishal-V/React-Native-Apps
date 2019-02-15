40ac5e49e3a630126e64122c48c8396dee95960e
Vishal-V <vishal114186@gmail.com>
2019-02-15 20:28:32 +0530

    Initial Render Template

--- weather/App.js
+++ weather/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native' ;
+import { 
+  Text, 
+  View, 
+  StyleSheet, 
+  Platform ,
+  KeyboardAvoidingView,
+  TextInput
+} from 'react-native' ;
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Render temporary!</Text>
+        <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
+        <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
+        <Text style={[styles.largeText, styles.textStyle]}>24°C</Text>
+        
       </View>
     );
   }
@@ -17,4 +27,25 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
   },
+  red: {
+    color: 'red',
+  },
+
+  textStyle: {
+    textAlign: 'center',
+    ...Platform.select({
+      ios: {
+        fontFamily: 'AvenirNext-Regular',
+      },
+      android: {
+        fontFamily: 'Roboto',
+      }
+    }),
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
+    },
 })
